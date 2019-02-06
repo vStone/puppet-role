c331144af69fdb3c2a7615de714ced4ba1840003
Jan Vansteenkiste <jan@vstone.eu>
2019-02-06 11:44:09 +0100

    Strip out callback support until upstream call() is fixed?

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,14 +14,13 @@
 #   * `trusted`: Use a trusted fact. The name must be configured using `trusted_extension_name`.
 #   * `fact`: Use a fact. The name must be configured using `fact_name`.
 #   * `param`: Uses the provided `role` parameter. This can also be used to configure using hiera.
-#   * `callback`: Use a function callback. The function must be configured using `function_callback_name`.
 #   * `default`: Fall back to the default value. You would typically put this after other methods.
 #   * `fail`: Fail the run if this method is reached. This enforces setting up a role and skips using the default role.
+#   * `callback`: BROKEN. DO NOT USE (yet). Use a function callback. The function must be configured using `function_callback_name`.
 #
 # @param role The role this node should get.
 # @param trusted_extension_name Name of the trusted fact (extension).
 # @param fact_name Name of the fact that contains the role.
-# @param function_callback_name A function that returns the role.
 #
 # @param default_role the default role to assume.
 # @param default_namespace namespace to use if the default is used.
@@ -35,7 +34,7 @@ class role (
   Optional[String[1]] $role                   = undef,
   Optional[String[1]] $trusted_extension_name = undef,
   Optional[String[1]] $fact_name              = undef,
-  Optional[String[1]] $function_callback_name = undef,
+  # Optional[String[1]] $function_callback_name = undef,
 
   String $default_role      = 'default',
   String $default_namespace = '::role',
@@ -71,15 +70,16 @@ class role (
           $resolved = undef
         }
       }
-      'callback': {
-        if $function_callback_name {
-          $deferred = Deferred($function_callback_name)
-          $resolved = $deferred.call()
-        }
-        else {
-          $resolved = undef
-        }
-      }
+      #'callback': {
+      #  if $function_callback_name {
+      #    $deferred = Deferred($function_callback_name)
+      #    $resolved = $deferred.call()
+      #    #$resolved = call($function_callback_name)
+      #    }
+      #    else {
+      #     $resolved = undef
+      #   }
+      # }
       'trusted': {
         if $trusted_extension_name and has_key($::trusted['extensions'], $trusted_extension_name) {
           $resolved = $::trusted['extensions'][$trusted_extension_name]
