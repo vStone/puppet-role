c11d07bc96786d999dacebc5f256552565f34dc6
Jan Vansteenkiste <jan@vstone.eu>
2019-07-15 14:19:50 +0200

    Add possibility to debug puppet while spec testing

---
+++ spec/spec_helper_local.rb
@@ -0,0 +1,8 @@
+if ENV['PUPPET_DEBUG']
+  RSpec.configure do |conf|
+    conf.before(:each) do
+      Puppet::Util::Log.level = :debug
+      Puppet::Util::Log.newdestination(:console)
+    end
+  end
+end
