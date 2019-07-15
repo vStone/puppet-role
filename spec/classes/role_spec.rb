650318ffda36b4681446bbf18d35cadd08ed4178
Jan Vansteenkiste <jan@vstone.eu>
2019-07-15 15:11:30 +0200

    Add search_namespaces.
    This allows you to specify multiple namespaces to look for a role in.

--- spec/classes/role_spec.rb
+++ spec/classes/role_spec.rb
@@ -13,6 +13,63 @@ describe 'role' do
   end
 
   context 'parameters' do
+    context '(search_)namespace(s)' do
+      let(:params) do
+        { resolve_order: 'param', role: 'param_role' }
+      end
+
+      context 'validate' do
+        describe 'neither provided' do
+          let(:params) { super() }
+
+          it do
+            is_expected.to compile.and_raise_error(%r{namespace or a not empty search_namespaces})
+          end
+        end
+        describe 'search_namespaces empty' do
+          let(:params) { super().merge(search_namespaces: []) }
+
+          it do
+            is_expected.to compile.and_raise_error(%r{namespace or a not empty search_namespaces})
+          end
+        end
+      end
+
+      {
+        'namespace' => { namespace: 'my_roles' },
+        'search_namespaces' => { search_namespaces: ['other_namespace', 'my_roles'] },
+        'both' => { namespace: 'my_roles', search_namespaces: ['other_namespace', 'other2'] },
+      }.each do |provided, parms|
+
+        describe "#{provided} provided" do
+          let(:params) do
+            super().merge(parms)
+          end
+
+          it do
+            is_expected.to compile
+            is_expected.to contain_class('my_roles::param_role')
+          end
+        end
+      end
+    end
+
+    describe 'separator' do
+      let(:pre_condition) do
+        'class role_bar() {}'
+      end
+      let(:params) do
+        {
+          separator: '_',
+          namespace: 'role',
+          resolve_order: ['param', 'default'],
+          role: 'bar',
+        }
+      end
+
+      it { is_expected.to contain_class('role_bar') }
+    end
+
     context 'resolve_order' do
       describe 'as single value' do
         let(:params) do
@@ -87,21 +144,67 @@ describe 'role' do
 
       it { is_expected.to contain_class('my_namespace::bar') }
     end
+  end
 
-    describe 'separator' do
-      let(:pre_condition) do
-        'class role_bar() {}'
-      end
+  context 'search namespaces configuration' do
+    let(:pre_condition) do
+      '
+      class my_namespace::spaced() {}
+      class my_otherspace::spaced() {}
+      class my_prefix_spaced() {}
+      class emptyspaced() {}
+      '
+    end
+
+    let(:params) do
+      {
+        separator: '::',
+        resolve_order: 'param',
+        role: 'spaced',
+      }
+    end
+
+    describe 'no match in namespaces' do
       let(:params) do
-        {
-          separator: '_',
-          namespace: 'role',
-          resolve_order: %w[param default],
-          role: 'bar',
-        }
+        super().merge(search_namespaces: ['does_not_exist', 'also_does_not_exist'])
       end
 
-      it { is_expected.to contain_class('role_bar') }
+      it do
+        is_expected.to compile.and_raise_error(%r{role '[^']*' not found on any of the search namespaces})
+      end
+    end
+
+    describe 'use first match' do
+      let(:params) do
+        super().merge(search_namespaces: ['my_otherspace', 'my_namespace'])
+      end
+
+      it do
+        is_expected.to compile
+        is_expected.to contain_class('my_otherspace::spaced')
+      end
+    end
+
+    describe 'double empty' do
+      let(:params) do
+        super().merge(role: 'emptyspaced', search_namespaces: [{ '' => '' }])
+      end
+
+      it do
+        is_expected.to compile
+        is_expected.to contain_class('emptyspaced')
+      end
+    end
+
+    describe 'with separator config' do
+      let(:params) do
+        super().merge(search_namespaces: [{ 'my_prefix' => '_' }])
+      end
+
+      it do
+        is_expected.to compile
+        is_expected.to contain_class('my_prefix_spaced')
+      end
     end
   end
 
