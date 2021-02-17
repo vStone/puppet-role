5f858e786125f5bb21e7668a99051d5759a368aa
Jan Vansteenkiste <jan@vstone.eu>
2021-02-17 12:28:17 +0100

    Add a usage notification when this module's default is used

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,2 +1,16 @@
 # Dummy default role that does nothing.
-class role::default {}
+class role::default {
+
+  notify {'The role module usage':
+    message => @("USAGE")
+    ==============================
+    Looks this node is using the default role. This means that the role module
+    probably not has been properly initialized.
+
+    You should override the default_prefix with your own prefix and optionally
+    supply a default_role.
+    ==============================
+    | - USAGE
+  }
+
+}
